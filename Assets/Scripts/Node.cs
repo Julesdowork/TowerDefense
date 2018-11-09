e8f906644a2563ae3cc814893450f6ed5a68f501
Julian McNeill <julesdowork@gmail.com>
2018-11-08 21:00:49 -0500

    Building

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private Renderer rend;
+    private Color startColor;
+    private GameObject turret;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+    void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Can't build there!");        // TODO display on-screen
+        }
+
+        // Build a turret
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+}
